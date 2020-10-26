303b6cf3f1c74629cd1790ad4ec28d7d5b74a5cf
tomimatsu0224yuki <tomimatsu0224yuki@gmail.com>
2020-10-26 15:33:00 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,14 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
-   end
+  end
 
-  #  def new
-  #  end
-
-   def create
+  def create
     Post.create(content: params[:content])
     redirect_to action: :index
-   end
+  end
 
-   def checked
+  def checked
     post = Post.find(params[:id])
     if post.checked 
       post.update(checked: false)
